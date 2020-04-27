624beec2666ed70927a38203940d3d9f252825c7
Jstay <jstay@Jstays-Mac-mini.local>
2020-04-27 10:23:24 +0300

    rounthomefromlogin

--- src/login/index.js
+++ src/login/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { View } from "react-native"
+import { useHistory } from "react-router-dom";
 
 //import all builder x files related to this directory
 import ComingSoon from "../modules/comingSoon";
@@ -7,30 +8,36 @@ import ComingSoon from "../modules/comingSoon";
 export default function Login(props) {
     const [componentIndex, setComponentIndex] = useState(0);
 
+    //this send user to route if they want to create a stay
+    let history = useHistory();
+    //user finished create a stay
+    function onSuccessfulLogin() {
+        history.push("/home");
+    };
 
     //add the import as a string to this array 
     //the array should be in the order that the screens show up
-    const componentKeys = ["ComingSoon"];
+    const componentKeys = ["Login"];
 
     return (
-        <View style={{backgroundColor: "green"}}>
+        <View style={{ backgroundColor: "green" }}>
             {/* copy paste below component*/}
-            
+
             {
                 //replace this string with the string 
                 //in componentKeys related to this import
-                
+
                 componentKeys[setComponentIndex] === "ComingSoon"
                 &&
                 //change component name to the new import 
                 <ComingSoon /*coming soon requires */ comingSoonPath="login"
-                    
+
                     //if builder x component has next button
                     //it's button should have onPress={()=>{props.onNext}}
-                    onNext={() => { 
+                    onNext={() => {
                         setComponentIndex(componentIndex + 1)
                     }}
-                    
+
                     //if builder x component has back button
                     //it's button should have onPress={()=>{props.onNext}}
                     onBack={() => {
