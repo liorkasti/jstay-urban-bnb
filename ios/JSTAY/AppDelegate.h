6074394f0de4094a68de9c7c4ce47d0f01f7a0de
nachumfreedman <nachumyf@gmail.com>
2020-08-12 13:47:35 +0300

    ios fix

--- ios/JSTAY/AppDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeDelegate.h>
-#import <UIKit/UIKit.h>
-#import <Firebase.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
-
-@end
